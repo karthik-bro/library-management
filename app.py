868fbb835a65b392bceea80e752e64056747c63c
karthik-bro <karthikgld683@gmail.com>
2026-03-29 01:08:22 +0530

    Fixed login return error

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def login():
         else:
             return "Invalid Credentials "
         
+    return render_template("login.html")
+        
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
